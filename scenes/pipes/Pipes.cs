f51583e2a3a66b675317470eb96c94f759b69331
Niamh <niamh.linguist@gmail.com>
2024-12-03 19:35:42 +0200

    pipes scroll across screen

---
+++ scenes/pipes/Pipes.cs
@@ -0,0 +1,24 @@
+using Godot;
+
+public partial class Pipes : Node2D
+{
+	private float SCROLL_SPEED = 120.0f;
+	[Export] private VisibleOnScreenNotifier2D _notifier;
+	private Vector2 _position;
+
+	public override void _Ready()
+	{
+		_notifier.ScreenExited += OnScreenExited;
+		_position = Position;
+	}
+
+	public override void _Process(double delta)
+	{
+		Position -= new Vector2(SCROLL_SPEED * (float)delta, 0.0f);
+	}
+
+	private void OnScreenExited()
+	{
+		QueueFree();
+	}
+}
